288416f7b3fd21da4eb0b75729c40c0e7262a897
Rushikesh <rushikesh0203@gmail.com>
2022-03-19 16:52:06 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "../css/Footer.css"
 export default function Footer(){
     return(
         <div className="footer">
-            <p>Made with ❤️ by Rushikesh. <a href="https://github.com/Heisenberg0203/" target="_blank">Github</a></p>
+            <p>Made with ❤️ by Rushikesh. <a href="https://github.com/Heisenberg0203/">Github</a></p>
         </div>
     );
 }
